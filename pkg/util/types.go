616d5fabcce9ec3641a920989e44652861afe063
Qian Sun <sunq@vmware.com>
2021-12-24 09:37:48 +0000

    Add constants for SecurityPolicy CR reconciling

---
+++ pkg/util/types.go
@@ -0,0 +1,18 @@
+/* Copyright © 2021 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0 */
+
+package util
+
+const (
+	TagScopeCluster              string = "nsx-op/cluster"
+	TagScopeNamespace            string = "nsx-op/namespace"
+	TagScopeSecurityPolicyCRName string = "nsx-op/security_policy_cr_name"
+	TagScopeSecurityPolicyCRUID  string = "nsx-op/security_policy_cr_uid"
+	TagScopeRuleID               string = "nsx-op/rule_id"
+	TagScopeGroupType            string = "nsx-op/group_type"
+	TagScopeNCPCluster           string = "ncp/cluster"
+	TagScopeNCPProject           string = "ncp/project"
+	TagScopeNCPVIFProject        string = "ncp/vif_project"
+	TagScopeNCPPod               string = "ncp/pod"
+	TagScopeNCPVNETInterface     string = "ncp/vnet_interface"
+)
