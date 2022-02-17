3bd1243cc027eddb218fc17a0d89036c1b128d6b
Tao Zou <tazou@vmware.com>
2022-02-17 20:07:02 +0800

    Fix not finding thumbprint
    Api manager in config may include port and scheme
    when searching local thumbprint, it should move the port part
    and only compare host

--- pkg/nsx/cluster_test.go
+++ pkg/nsx/cluster_test.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"strings"
 	"testing"
 
+	"github.com/agiledragon/gomonkey"
 	"github.com/stretchr/testify/assert"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/ratelimiter"
 )
@@ -30,4 +32,44 @@ func TestNewCluster(t *testing.T) {
 	config := NewConfig(a, "admin", "passw0rd", "", 10, 3, 20, 20, true, true, true, ratelimiter.AIMD, nil, nil, thumbprint)
 	_, err := NewCluster(config)
 	assert.True(t, err == nil, fmt.Sprintf("Created cluster failed %v", err))
+}
+
+func TestCluster_getThumbprint(t *testing.T) {
+	// one api server, one thumbprint
+	thumbprint := []string{"123"}
+	host := "127.0.0.1:443"
+
+	cluster := &Cluster{}
+	cluster.config = &Config{}
+	cluster.config.Thumbprint = thumbprint
+	tb := cluster.getThumbprint(host)
+	assert.Equal(t, tb, "123")
+
+	// two api server share one thumbprint
+	ep1 := &Endpoint{}
+	ep2 := &Endpoint{}
+	patch := gomonkey.ApplyMethod(reflect.TypeOf(ep1), "KeepAlive", func(_ *Endpoint) {
+	})
+	defer patch.Reset()
+	cluster.endpoints = []*Endpoint{ep1, ep2}
+	cluster.endpoints[0].provider = &address{host: "127.0.0.1:443"}
+	cluster.endpoints[1].provider = &address{host: "127.0.0.2:443"}
+	tb = cluster.getThumbprint(host)
+	assert.Equal(t, tb, "123")
+
+	//two api server, two thumbprint
+	thumbprint = []string{"123", "234"}
+	cluster.config.Thumbprint = thumbprint
+	tb = cluster.getThumbprint("127.0.0.1:443")
+	assert.Equal(t, tb, "123")
+	tb = cluster.getThumbprint("127.0.0.2:443")
+	assert.Equal(t, tb, "234")
+
+	//two api server no port, two thumbprint
+	cluster.endpoints[0].provider = &address{host: "127.0.0.1"}
+	cluster.endpoints[1].provider = &address{host: "127.0.0.2"}
+	tb = cluster.getThumbprint("127.0.0.1:443")
+	assert.Equal(t, tb, "123")
+	tb = cluster.getThumbprint("127.0.0.2:443")
+	assert.Equal(t, tb, "234")
 }
