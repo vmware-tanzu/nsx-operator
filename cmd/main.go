zhengxiexie/nsx-operator-1

Refactor common module from service and controller and re-organize directory
Considering more CRDs come in, extract some common functions
and structs we could reuse or extend from.

cmd/main.go
@@ -18,11 +18,12 @@ import (
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
 	"github.com/vmware-tanzu/nsx-operator/pkg/config"
-	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/securitypolicy"
+	securitypolicycontrollers "github.com/vmware-tanzu/nsx-operator/pkg/controllers/securitypolicy"
 	"github.com/vmware-tanzu/nsx-operator/pkg/logger"
 	"github.com/vmware-tanzu/nsx-operator/pkg/metrics"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx"
-	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services"
+	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/common"
+	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/securitypolicy"
 )
 
 var (
@@ -52,6 +53,23 @@ func init() {
 	}
 }
 
+func StartSecurityPolicyController(mgr ctrl.Manager, commonService common.Service) {
+	securityReconcile := &securitypolicycontrollers.SecurityPolicyReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}
+	if securityService, err := securitypolicy.InitializeSecurityPolicy(commonService); err != nil {
+		log.Error(err, "failed to initialize securitypolicy commonService", "controller", "SecurityPolicy")
+		os.Exit(1)
+	} else {
+		securityReconcile.Service = securityService
+	}
+	if err := securityReconcile.Start(mgr); err != nil {
+		log.Error(err, "failed to create controller", "controller", "SecurityPolicy")
+		os.Exit(1)
+	}
+}
+
 func main() {
 	log.Info("starting NSX Operator")
 
@@ -65,27 +83,23 @@ func main() {
 		log.Error(err, "failed to init manager")
 		os.Exit(1)
 	}
-	securityReconcile := &securitypolicy.SecurityPolicyReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}
+
+	// nsxClient is used to interact with NSX API.
 	nsxClient := nsx.GetClient(cf)
 	if nsxClient == nil {
 		log.Error(err, "failed to get nsx client")
 		os.Exit(1)
 	}
-	if service, err := services.InitializeSecurityPolicy(nsxClient, cf); err != nil {
-		log.Error(err, "failed to initialize securitypolicy service", "controller", "SecurityPolicy")
-		os.Exit(1)
-	} else {
-		service.Client = mgr.GetClient()
-		securityReconcile.Service = service
+
+	//  Embed the common commonService to sub-services.
+	var commonService = common.Service{
+		Client:    mgr.GetClient(),
+		NSXClient: nsxClient,
+		NSXConfig: cf,
 	}
 
-	if err = securityReconcile.Start(mgr); err != nil {
-		log.Error(err, "failed to create controller", "controller", "SecurityPolicy")
-		os.Exit(1)
-	}
+	// Start the security policy controller.
+	StartSecurityPolicyController(mgr, commonService)
 
 	if metrics.AreMetricsExposed(cf) {
 		go updateHealthMetricsPeriodically(nsxClient)

pkg/nsx/services/common/store.go
@@ -1,0 +1,87 @@
+package common
+
+import (
+	error2 "errors"
+
+	"github.com/vmware/vsphere-automation-sdk-go/lib/vapi/std/errors"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
+	"k8s.io/client-go/tools/cache"
+
+	util2 "github.com/vmware-tanzu/nsx-operator/pkg/nsx/util"
+)
+
+// IndexFunc is wrapper of cache.IndexFunc, used by cache.Indexers
+func IndexFunc(scope string) cache.IndexFunc {
+	return func(obj interface{}) ([]string, error) {
+		return CRUIDScopeIndexFunc(scope, obj)
+	}
+}
+
+// CRUIDScopeIndexFunc returns the index of the object.
+// The index is the value of the tag with scope CRUID, the unique in
+// time and space value for this object at k8s side, which is generated by k8s.
+func CRUIDScopeIndexFunc(scope string, obj interface{}) ([]string, error) {
+	var tags []model.Tag
+	switch v := obj.(type) {
+	case model.SecurityPolicy:
+		tags = v.Tags
+	case model.Group:
+		tags = v.Tags
+	case model.Rule:
+		tags = v.Tags
+	default:
+		return nil, error2.New("invalid object type")
+	}
+	return filterTag(scope, tags), nil
+}
+
+func filterTag(scope string, v []model.Tag) []string {
+	res := make([]string, 0, 5)
+	for _, tag := range v {
+		if *tag.Scope == scope {
+			res = append(res, *tag.Tag)
+		}
+	}
+	return res
+}
+
+// KeyFunc returns the key of the object.
+func KeyFunc(obj interface{}) (string, error) {
+	switch v := obj.(type) {
+	case model.Group:
+		return *v.Id, nil
+	case model.SecurityPolicy:
+		return *v.Id, nil
+	case model.Rule:
+		return *v.Id, nil
+	}
+	return "", nil
+}
+
+func DecrementPageSize(pageSize *int64) {
+	*pageSize -= 100
+	if int(*pageSize) <= 0 {
+		*pageSize = 10
+	}
+}
+
+func TransError(err error) error {
+	switch err.(type) {
+	case errors.ServiceUnavailable:
+		vApiError, _ := err.(errors.ServiceUnavailable)
+		if vApiError.Data == nil {
+			return err
+		}
+		data, errs := Converter.ConvertToGolang(vApiError.Data, model.ApiErrorBindingType())
+		if len(errs) > 0 {
+			return err
+		}
+		apiError := data.(model.ApiError)
+		if *apiError.ErrorCode == int64(60576) {
+			return util2.PageMaxError{Desc: "page max overflow"}
+		}
+	default:
+		return err
+	}
+	return err
+}

vmware-tanzu/nsx-operator

cmd/main.go
@@ -8,27 +8,26 @@ import (
 	"os"
 	"time"
 
-	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
 	"github.com/vmware-tanzu/nsx-operator/pkg/config"
-	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/securitypolicy"
+	securitypolicycontroller "github.com/vmware-tanzu/nsx-operator/pkg/controllers/securitypolicy"
 	"github.com/vmware-tanzu/nsx-operator/pkg/logger"
 	"github.com/vmware-tanzu/nsx-operator/pkg/metrics"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx"
-	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services"
+	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/common"
+	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/securitypolicy"
 )
 
 var (
 	scheme                 = runtime.NewScheme()
 	probeAddr, metricsAddr string
-	log                    logr.Logger
+	log                    = logger.Log
 	cf                     *config.NSXOperatorConfig
 )
 
@@ -45,10 +44,25 @@ func init() {
 	if err != nil {
 		os.Exit(1)
 	}
-	logf.SetLogger(logger.ZapLogger())
-	log = logf.Log.WithName("main")
 	if metrics.AreMetricsExposed(cf) {
 		metrics.InitializePrometheusMetrics()
+	}
+}
+
+func StartSecurityPolicyController(mgr ctrl.Manager, commonService common.Service) {
+	securityReconcile := &securitypolicycontroller.SecurityPolicyReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}
+	if securityService, err := securitypolicy.InitializeSecurityPolicy(commonService); err != nil {
+		log.Error(err, "failed to initialize securitypolicy commonService", "controller", "SecurityPolicy")
+		os.Exit(1)
+	} else {
+		securityReconcile.Service = securityService
+	}
+	if err := securityReconcile.Start(mgr); err != nil {
+		log.Error(err, "failed to create controller", "controller", "SecurityPolicy")
+		os.Exit(1)
 	}
 }
 
@@ -65,27 +79,23 @@ func main() {
 		log.Error(err, "failed to init manager")
 		os.Exit(1)
 	}
-	securityReconcile := &securitypolicy.SecurityPolicyReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}
+
+	// nsxClient is used to interact with NSX API.
 	nsxClient := nsx.GetClient(cf)
 	if nsxClient == nil {
 		log.Error(err, "failed to get nsx client")
 		os.Exit(1)
 	}
-	if service, err := services.InitializeSecurityPolicy(nsxClient, cf); err != nil {
-		log.Error(err, "failed to initialize securitypolicy service", "controller", "SecurityPolicy")
-		os.Exit(1)
-	} else {
-		service.Client = mgr.GetClient()
-		securityReconcile.Service = service
+
+	//  Embed the common commonService to sub-services.
+	var commonService = common.Service{
+		Client:    mgr.GetClient(),
+		NSXClient: nsxClient,
+		NSXConfig: cf,
 	}
 
-	if err = securityReconcile.Start(mgr); err != nil {
-		log.Error(err, "failed to create controller", "controller", "SecurityPolicy")
-		os.Exit(1)
-	}
+	// Start the security policy controller.
+	StartSecurityPolicyController(mgr, commonService)
 
 	if metrics.AreMetricsExposed(cf) {
 		go updateHealthMetricsPeriodically(nsxClient)

pkg/nsx/services/common/store.go
@@ -1,0 +1,87 @@
+package common
+
+import (
+	"errors"
+
+	vapierrors "github.com/vmware/vsphere-automation-sdk-go/lib/vapi/std/errors"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
+	"k8s.io/client-go/tools/cache"
+
+	nsxutil "github.com/vmware-tanzu/nsx-operator/pkg/nsx/util"
+)
+
+// IndexFunc is wrapper of cache.IndexFunc, used by cache.Indexers
+func IndexFunc(scope string) cache.IndexFunc {
+	return func(obj interface{}) ([]string, error) {
+		return CRUIDScopeIndexFunc(scope, obj)
+	}
+}
+
+// CRUIDScopeIndexFunc returns the index of the object.
+// The index is the value of the tag with scope CRUID, the unique in
+// time and space value for this object at k8s side, which is generated by k8s.
+func CRUIDScopeIndexFunc(scope string, obj interface{}) ([]string, error) {
+	var tags []model.Tag
+	switch v := obj.(type) {
+	case model.SecurityPolicy:
+		tags = v.Tags
+	case model.Group:
+		tags = v.Tags
+	case model.Rule:
+		tags = v.Tags
+	default:
+		return nil, errors.New("invalid object type")
+	}
+	return filterTag(scope, tags), nil
+}
+
+func filterTag(scope string, v []model.Tag) []string {
+	res := make([]string, 0, 5)
+	for _, tag := range v {
+		if *tag.Scope == scope {
+			res = append(res, *tag.Tag)
+		}
+	}
+	return res
+}
+
+// KeyFunc returns the key of the object.
+func KeyFunc(obj interface{}) (string, error) {
+	switch v := obj.(type) {
+	case model.Group:
+		return *v.Id, nil
+	case model.SecurityPolicy:
+		return *v.Id, nil
+	case model.Rule:
+		return *v.Id, nil
+	}
+	return "", nil
+}
+
+func DecrementPageSize(pageSize *int64) {
+	*pageSize -= 100
+	if int(*pageSize) <= 0 {
+		*pageSize = 10
+	}
+}
+
+func TransError(err error) error {
+	switch err.(type) {
+	case vapierrors.ServiceUnavailable:
+		vApiError, _ := err.(vapierrors.ServiceUnavailable)
+		if vApiError.Data == nil {
+			return err
+		}
+		data, errs := Converter.ConvertToGolang(vApiError.Data, model.ApiErrorBindingType())
+		if len(errs) > 0 {
+			return err
+		}
+		apiError := data.(model.ApiError)
+		if *apiError.ErrorCode == int64(60576) {
+			return nsxutil.PageMaxError{Desc: "page max overflow"}
+		}
+	default:
+		return err
+	}
+	return err
+}