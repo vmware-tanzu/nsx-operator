e07563465ee0b69c0f6f66d5ecc4a98fdc341917
Xie Zheng <xizheng@vmware.com>
2022-11-21 11:46:39 +0800

    Add TestSecurityPolicyBasicTraffic e2e test

--- test/e2e/nsx_security_policy_test.go
+++ test/e2e/nsx_security_policy_test.go
@@ -10,8 +10,8 @@ import (
 // TestSecurityPolicyBasicTraffic verifies that the new created pod appears in inventory.
 func TestSecurityPolicyBasicTraffic(t *testing.T) {
 	ns := "test-security-policy-1"
-	//setupTest(t, ns)
-	//defer teardownTest(t, ns)
+	setupTest(t, ns)
+	defer teardownTest(t, ns)
 
 	busyboxPath, _ := filepath.Abs("./manifest/testSecurityPolicy/busybox.yaml")
 	_ = applyYAML(busyboxPath, ns)
@@ -24,4 +24,15 @@ func TestSecurityPolicyBasicTraffic(t *testing.T) {
 	iPs, err := testData.podWaitForIPs(defaultTimeout, "nc-pod", ns)
 	t.Logf("Pods are %v", iPs)
 	assert.Nil(t, err, "Error when waiting for IP for Pod 'nc-pod'")
+
+	err = testData.runPingCommandFromTestPod(ns, "busybox", iPs, 4)
+	assert.Nil(t, err, "Error when running ping command from test Pod 'busybox'")
+
+	nsIsolationPath, _ := filepath.Abs("./manifest/testSecurityPolicy/ns-isolation-policy.yaml")
+	_ = applyYAML(nsIsolationPath, ns)
+	err = testData.securityPolicyWaitFor(defaultTimeout, "isolate-policy-1", ns)
+	assert.Nil(t, err, "Error when waiting for Security Policy 'isolate-policy-1'")
+
+	err = testData.runPingCommandFromTestPod(ns, "busybox", iPs, 4)
+	assert.NotNilf(t, err, "Error when running ping command from test Pod 'busybox'")
 }
