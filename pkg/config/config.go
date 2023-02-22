b6ea06ee18247b9cd97ab9afca735ac288d40185
lxiaopei <lxiaopei@vmware.com>
2023-02-22 17:51:33 +0800

    Add VPC related options in NsxConfig
    Add VPC related options in NsxConfig.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -81,7 +81,8 @@ type DefaultConfig struct {
 }
 
 type CoeConfig struct {
-	Cluster string `ini:"cluster"`
+	Cluster          string `ini:"cluster"`
+	EnableVPCNetwork bool   `ini:"enable_vpc_network"`
 }
 
 type NsxConfig struct {
@@ -95,6 +96,9 @@ type NsxConfig struct {
 	Insecure             bool     `ini:"insecure"`
 	SingleTierSrTopology bool     `ini:"single_tier_sr_topology"`
 	EnforcementPoint     string   `ini:"enforcement_point"`
+	DefaultProject       string   `ini:"default_project"`
+	ExternalIPv4Blocks   []string `ini:"external_ipv4_blocks"`
+	DefaultSubnetSize    int      `ini:"default_subnet_size"`
 }
 
 type K8sConfig struct {
@@ -174,7 +178,7 @@ func LoadConfigFromFile() (*NSXOperatorConfig, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	
 	if err := nsxOperatorConfig.validate(); err != nil {
 		return nil, err
 	}
@@ -338,6 +342,14 @@ func (nsxConfig *NsxConfig) validate() error {
 	}
 	if err := nsxConfig.validateCert(); err != nil {
 		return err
+	}
+	enableVPC := coeConfig.EnableVPCNetwork
+	if enableVPC {
+		if nsxConfig.DefaultProject == "" || len(nsxConfig.ExternalIPv4Blocks) == 0 {
+			err := errors.New("default_project is none or external_ipv4_blocks is empty")
+			log.Error(err, "validate VPCConfig failed")
+			return err
+		}
 	}
 	return nil
 }
