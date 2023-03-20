259fe7b6d0feaa2ac177828a2875c302c5edff5f
Xie Zheng <xizheng@vmware.com>
2023-03-20 14:36:17 +0800

    Add GetOrgProject method to ServiceMediator
    Create the common method used by other modules. When using gomonkey to test the method, encounter the problem that
    the mocked method doesn't take effect, fix it by adding -gcflags=all=-l option to prohibit inline optimization.

--- pkg/nsx/services/mediator/mediator.go
+++ pkg/nsx/services/mediator/mediator.go
@@ -1,8 +1,17 @@
 package mediator
 
 import (
+	"regexp"
+
+	"github.com/vmware-tanzu/nsx-operator/pkg/logger"
+	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/common"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/securitypolicy"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/vpc"
+)
+
+var (
+	log   = logger.Log
+	reExp = regexp.MustCompile(`/orgs/([^/]+)/projects/([^/]+)/vpcs/([^/]+)`)
 )
 
 // ServiceMediator We use mediator pattern to wrap all the services,
@@ -13,4 +22,26 @@ import (
 type ServiceMediator struct {
 	*securitypolicy.SecurityPolicyService
 	*vpc.VPCService
+}
+
+// GetOrgProjectVPC is a common method, extracting the org and project string from vpc path of the VPC model.
+// VPC path looks like "/orgs/default/projects/project-1/vpcs/vpc-1",
+// Since other modules only know namespace, this is the only entry point to get org and project.
+// Currently, we only support one vpc per namespace, but we may support multiple vpcs per namespace in the future,
+// so we return a slice of OrgProjectVPC.
+func (serviceMediator *ServiceMediator) GetOrgProjectVPC(ns string) []common.OrgProjectVPC {
+	var orgProjectVPC []common.OrgProjectVPC
+	vpcs := serviceMediator.GetVPCsByNamespace(ns) // Transparently call the VPCService.GetVPCsByNamespace method
+	for _, v := range vpcs {
+		matches := reExp.FindStringSubmatch(*v.Path)
+		if len(matches) == 4 {
+			org := matches[1]
+			project := matches[2]
+			vpc_ := matches[3]
+			orgProjectVPC = append(orgProjectVPC, common.OrgProjectVPC{OrgID: org, ProjectID: project, VPCID: vpc_})
+		} else {
+			log.Error(nil, "Failed to get org and project from vpc path", "vpc path", *v.Path)
+		}
+	}
+	return orgProjectVPC
 }
