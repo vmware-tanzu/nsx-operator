1803f2fdb56ef4cd343a57729efe7940d2422bd6
Xie Zheng <xizheng@vmware.com>
2023-08-07 17:12:43 +0800

    Refactor loglevel based on debug in configmap
    We hold these truths to be self-evident:
    log.info -> info, log.v(1).info -> debug, log.error-> error, log.v(2).info-> develop level log
    
    If config.Debug=true, then loglevel=1, if command is passed in loglevel and greater than debug loglevel,
    then the loglevel is overridden.
    Use another log in config.go.

--- cmd_clean/main.go
+++ cmd_clean/main.go
@@ -63,6 +63,7 @@ func main() {
 	flag.IntVar(&config.LogLevel, "log-level", 0, "Use zap-core log system.")
 	flag.Parse()
 
+	logf.SetLogger(logger.ZapLogger())
 	cf = config.NewNSXOpertorConfig()
 	cf.NsxApiManagers = []string{mgrIp}
 	cf.VCUser = vcUser
