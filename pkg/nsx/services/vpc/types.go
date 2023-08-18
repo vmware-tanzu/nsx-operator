2dc2361bc877ed9bd3bdf3a992bfeebb69055129
Sean Pang <ranp@vmware.com>
2023-08-18 15:01:27 +0800

    Support appending cidr to network config CR

--- pkg/nsx/services/vpc/types.go
+++ pkg/nsx/services/vpc/types.go
@@ -1,12 +1,13 @@
 package vpc
 
 type VPCNetworkConfigInfo struct {
-	Name                    string
-	DefaultGatewayPath      string
-	EdgeClusterPath         string
-	NsxtProject             string
-	ExternalIPv4Blocks      []string
-	PrivateIPv4CIDRs        []string
-	DefaultIPv4SubnetSize   int
-	DefaultSubnetAccessMode string
+	Name                           string
+	DefaultGatewayPath             string
+	EdgeClusterPath                string
+	NsxtProject                    string
+	ExternalIPv4Blocks             []string
+	PrivateIPv4CIDRs               []string
+	DefaultIPv4SubnetSize          int
+	DefaultSubnetAccessMode        string
+	LoadBalancerVPCEndpointEnabled bool
 }
