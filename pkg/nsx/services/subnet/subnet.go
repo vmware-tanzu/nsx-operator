512fd523051931dcb59a89871c4ca7b86ded095b
Xie Zheng <xizheng@vmware.com>
2024-09-26 14:40:08 +0800

    Fix bug which would create duplicated subnets when realization fails
    Subnets are allocated from subnet sets, it's id is suffixed with a random string, if the subnet failed to realize, and not being deleted, then a new subnet with different suffix will be created, thus leading a mount of duplicated subnets being left in the system.
    
    Signed-off-by: Xie Zheng <xizheng@vmware.com>

--- pkg/nsx/services/subnet/subnet.go
+++ pkg/nsx/services/subnet/subnet.go
@@ -128,6 +128,12 @@ func (service *SubnetService) createOrUpdateSubnet(obj client.Object, nsxSubnet 
 	}
 	if err = realizeService.CheckRealizeState(backoff, *nsxSubnet.Path, "RealizedLogicalSwitch"); err != nil {
 		log.Error(err, "failed to check subnet realization state", "ID", *nsxSubnet.Id)
+		// Delete the subnet if realization check fails, avoiding creating duplicate subnets continuously.
+		deleteErr := service.DeleteSubnet(*nsxSubnet)
+		if deleteErr != nil {
+			log.Error(deleteErr, "failed to delete subnet after realization check failure", "ID", *nsxSubnet.Id)
+			return "", fmt.Errorf("realization check failed: %v; deletion failed: %v", err, deleteErr)
+		}
 		return "", err
 	}
 	if err = service.SubnetStore.Apply(nsxSubnet); err != nil {
