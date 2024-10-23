c36d42316c939d2b85770e247bd7f9978d0b2dc6
wenqi <wenqiq@vmware.com>
2024-10-23 13:32:46 +0800

    Add unit tests for Subnet controller (#806)
    Signed-off-by: Wenqi Qiu <wenqiq@vmware.com>

--- pkg/controllers/subnet/namespace_handler_test.go
+++ pkg/controllers/subnet/namespace_handler_test.go
@@ -3,126 +3,159 @@ package subnet
 import (
 	"context"
 	"testing"
-	"time"
 
+	"github.com/agiledragon/gomonkey/v2"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/util/workqueue"
+	ctlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/vpc/v1alpha1"
 )
 
-type MockRateLimitingInterface struct {
-	mock.Mock
+func TestEnqueueRequestForNamespace_Create(t *testing.T) {
+	client := fake.NewClientBuilder().Build()
+	e := &EnqueueRequestForNamespace{Client: client}
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	e.Create(context.TODO(), event.CreateEvent{}, queue)
+	// No asserts here because Create does nothing, just ensuring no errors.
 }
 
-func (m *MockRateLimitingInterface) Add(item interface{}) {
+func TestEnqueueRequestForNamespace_Delete(t *testing.T) {
+	client := fake.NewClientBuilder().Build()
+	e := &EnqueueRequestForNamespace{Client: client}
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	e.Delete(context.TODO(), event.DeleteEvent{}, queue)
+	// No asserts here because Delete does nothing, just ensuring no errors.
 }
 
-func (m *MockRateLimitingInterface) Len() int {
-	return 0
+func TestEnqueueRequestForNamespace_Generic(t *testing.T) {
+	client := fake.NewClientBuilder().Build()
+	e := &EnqueueRequestForNamespace{Client: client}
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	e.Generic(context.TODO(), event.GenericEvent{}, queue)
+	// No asserts here because Generic does nothing, just ensuring no errors.
 }
 
-func (m *MockRateLimitingInterface) Get() (item interface{}, shutdown bool) {
-	return
-}
+func TestEnqueueRequestForNamespace_Update(t *testing.T) {
+	// Prepare test data
+	oldNamespace := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "test-namespace",
+			Labels: map[string]string{"env": "test"},
+		},
+	}
 
-func (m *MockRateLimitingInterface) Done(item interface{}) {
-	return
-}
+	newNamespace := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "test-namespace",
+			Labels: map[string]string{"env": "prod"},
+		},
+	}
 
-func (m *MockRateLimitingInterface) ShutDown() {
-}
+	client := fake.NewClientBuilder().WithObjects(newNamespace).Build()
+	e := &EnqueueRequestForNamespace{Client: client}
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
 
-func (m *MockRateLimitingInterface) ShutDownWithDrain() {
-}
+	updateEvent := event.UpdateEvent{
+		ObjectOld: oldNamespace,
+		ObjectNew: newNamespace,
+	}
 
-func (m *MockRateLimitingInterface) ShuttingDown() bool {
-	return true
-}
+	subnet := v1alpha1.Subnet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-subnet",
+			Namespace: "test-namespace",
+		},
+	}
+	subnetList := &v1alpha1.SubnetList{
+		TypeMeta: metav1.TypeMeta{},
+		ListMeta: metav1.ListMeta{},
+		Items:    []v1alpha1.Subnet{subnet},
+	}
 
-func (m *MockRateLimitingInterface) AddAfter(item interface{}, duration time.Duration) {
-	return
-}
+	e.Update(context.TODO(), updateEvent, queue)
+	assert.Equal(t, 0, queue.Len(), "Expected 1 item to be requeued")
 
-func (m *MockRateLimitingInterface) AddRateLimited(item interface{}) {
-	m.Called(item)
-}
-
-func (m *MockRateLimitingInterface) Forget(item interface{}) {
-	m.Called(item)
-}
-
-func (m *MockRateLimitingInterface) NumRequeues(item interface{}) int {
-	args := m.Called(item)
-	return args.Int(0)
-}
-
-func TestEnqueueRequestForNamespace(t *testing.T) {
-	fakeClient := fake.NewClientBuilder().Build()
-	queue := new(MockRateLimitingInterface)
-	handler := &EnqueueRequestForNamespace{Client: fakeClient}
-
-	t.Run("Update event with changed labels", func(t *testing.T) {
-		oldNamespace := &v1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:   "test-ns",
-				Labels: map[string]string{"key": "old"},
-			},
-		}
-		newNamespace := &v1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:   "test-ns",
-				Labels: map[string]string{"key": "new"},
-			},
-		}
-		updateEvent := event.UpdateEvent{
-			ObjectOld: oldNamespace,
-			ObjectNew: newNamespace,
-		}
-		queue.On("Add", mock.Anything).Return()
-
-		handler.Update(context.Background(), updateEvent, queue)
+	patches := gomonkey.ApplyFunc(listSubnet, func(c ctlclient.Client, ctx context.Context, options ...ctlclient.ListOption) (*v1alpha1.SubnetList, error) {
+		return subnetList, nil
 	})
+	defer patches.Reset()
 
-	t.Run("Update event with unchanged labels", func(t *testing.T) {
-		oldNamespace := &v1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:   "test-ns",
-				Labels: map[string]string{"key": "same"},
-			},
-		}
-		newNamespace := &v1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:   "test-ns",
-				Labels: map[string]string{"key": "same"},
-			},
-		}
-		updateEvent := event.UpdateEvent{
-			ObjectOld: oldNamespace,
-			ObjectNew: newNamespace,
-		}
+	e.Update(context.TODO(), updateEvent, queue)
+	assert.Equal(t, 1, queue.Len(), "Expected 1 item to be requeued")
+}
 
-		queue.On("Add", mock.Anything).Return()
+func TestPredicateFuncsNs_UpdateFunc(t *testing.T) {
+	oldNamespace := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "test-namespace",
+			Labels: map[string]string{"env": "test"},
+		},
+	}
 
-		handler.Update(context.Background(), updateEvent, queue)
+	newNamespace := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "test-namespace",
+			Labels: map[string]string{"env": "prod"},
+		},
+	}
 
-		queue.AssertNotCalled(t, "Add", mock.Anything)
-	})
+	updateEvent := event.UpdateEvent{
+		ObjectOld: oldNamespace,
+		ObjectNew: newNamespace,
+	}
 
-	t.Run("Requeue subnet function", func(t *testing.T) {
-		ns := "test-ns"
+	// Test the update function logic in PredicateFuncsNs
+	result := PredicateFuncsNs.UpdateFunc(updateEvent)
+	assert.True(t, result, "Expected update event to trigger requeue")
 
-		schem := fake.NewClientBuilder().Build().Scheme()
-		v1alpha1.AddToScheme(schem)
-		queue.On("Add", mock.Anything).Return()
+	// Test with no label change
+	noChangeEvent := event.UpdateEvent{
+		ObjectOld: oldNamespace,
+		ObjectNew: oldNamespace,
+	}
 
-		err := requeueSubnet(fakeClient, ns, queue)
+	result = PredicateFuncsNs.UpdateFunc(noChangeEvent)
+	assert.False(t, result, "Expected no action when labels have not changed")
 
-		assert.NoError(t, err)
-		queue.AssertNumberOfCalls(t, "Add", 0)
-	})
+	res := PredicateFuncsNs.CreateFunc(event.CreateEvent{Object: newNamespace})
+	assert.False(t, res, "Expected no action when labels have not changed")
+
+	res = PredicateFuncsNs.DeleteFunc(event.DeleteEvent{Object: newNamespace})
+	assert.False(t, res, "Expected no action when labels have not changed")
+}
+
+func TestRequeueSubnetSet(t *testing.T) {
+	// Prepare test data
+	subnet := &v1alpha1.Subnet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-subnet",
+			Namespace: "test-namespace",
+		},
+	}
+
+	scheme := clientgoscheme.Scheme
+	v1alpha1.AddToScheme(scheme)
+
+	// Test for empty namespace (no SubnetSets found)
+	emptyClient := fake.NewClientBuilder().Build()
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+	err := requeueSubnet(emptyClient, "empty-namespace", queue)
+	assert.NoError(t, err, "Expected no error with empty namespace")
+	assert.Equal(t, 0, queue.Len(), "Expected no items to be requeued for empty namespace")
+
+	client := fake.NewClientBuilder().WithScheme(scheme).WithObjects(subnet).Build()
+	queue = workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	err = requeueSubnet(client, "test-namespace", queue)
+	assert.NoError(t, err, "Expected no error while requeueing SubnetSets")
+	assert.Equal(t, 1, queue.Len(), "Expected 1 item to be requeued")
 }
