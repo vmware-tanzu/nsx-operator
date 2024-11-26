509290d0e8830a14b092b385245d3b0fb25cda37
Wenying Dong <wenyingd@vmware.com>
2024-11-26 16:21:47 +0800

    [e2e] Add test cases for pre-created VPC

--- test/e2e/framework.go
+++ test/e2e/framework.go
@@ -15,14 +15,18 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
 	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-
-	"github.com/vmware-tanzu/nsx-operator/pkg/logger"
+	"k8s.io/client-go/tools/remotecommand"
+	"k8s.io/utils/ptr"
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/client/clientset/versioned"
+	"github.com/vmware-tanzu/nsx-operator/pkg/logger"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/common"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/util"
 	"github.com/vmware-tanzu/nsx-operator/test/e2e/providers"
@@ -255,11 +259,14 @@ func collectClusterInfo() error {
 }
 
 // createNamespace creates the provided namespace.
-func (data *TestData) createNamespace(namespace string) error {
+func (data *TestData) createNamespace(namespace string, mutators ...func(ns *corev1.Namespace)) error {
 	ns := corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: namespace,
 		},
+	}
+	for _, mutator := range mutators {
+		mutator(&ns)
 	}
 	if ns, err := data.clientset.CoreV1().Namespaces().Create(context.TODO(), &ns, metav1.CreateOptions{}); err != nil {
 		// Ignore error if the namespace already exists
@@ -332,8 +339,12 @@ func (data *TestData) deploymentWaitForNames(timeout time.Duration, namespace, d
 	return podNames, nil
 }
 
-// Temporarily disable traffic check
-/*
+type PodIPs struct {
+	ipv4      *net.IP
+	ipv6      *net.IP
+	ipStrings []string
+}
+
 // podWaitFor polls the K8s apiServer until the specified Pod is found (in the test Namespace) and
 // the condition predicate is met (or until the provided timeout expires).
 func (data *TestData) podWaitFor(timeout time.Duration, name, namespace string, condition PodCondition) (*corev1.Pod, error) {
@@ -397,6 +408,7 @@ func (data *TestData) podWaitForIPs(timeout time.Duration, name, namespace strin
 	return ips, nil
 }
 
+/*
 // deploymentWaitForIPsOrNames polls the K8s apiServer until the specified Pod in deployment has an IP address
 func (data *TestData) deploymentWaitForIPsOrNames(timeout time.Duration, namespace, deployment string) ([]string, []string, error) {
 	podIPStrings := sets.NewString()
@@ -429,6 +441,7 @@ func (data *TestData) deploymentWaitForIPsOrNames(timeout time.Duration, namespa
 	}
 	return podIPStrings.List(), podNames, nil
 }
+*/
 
 func parsePodIPs(podIPStrings sets.Set[string]) (*PodIPs, error) {
 	ips := new(PodIPs)
@@ -493,6 +506,7 @@ func (data *TestData) runCommandFromPod(namespace string, podName string, contai
 	return stdoutB.String(), stderrB.String(), nil
 }
 
+/*
 func (data *TestData) runPingCommandFromPod(namespace string, podName string, targetPodIPs *PodIPs, count int) error {
 	var cmd []string
 	if targetPodIPs.ipv4 != nil {
@@ -686,5 +700,104 @@ func (data *TestData) waitForResourceExistByPath(pathPolicy string, shouldExist 
 		}
 		return true, nil
 	})
+	return err
+}
+
+func (data *TestData) createService(namespace, serviceName string, port, targetPort int32, protocol corev1.Protocol, selector map[string]string,
+	serviceType corev1.ServiceType, mutators ...func(service *corev1.Service)) (*corev1.Service, error) {
+	ipFamilies := []corev1.IPFamily{corev1.IPv4Protocol}
+
+	service := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceName,
+			Namespace: namespace,
+			Labels: map[string]string{
+				"nsx-op-e2e": serviceName,
+				"app":        serviceName,
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			SessionAffinity: corev1.ServiceAffinityNone,
+			Ports: []corev1.ServicePort{{
+				Port:       port,
+				TargetPort: intstr.FromInt32(targetPort),
+				Protocol:   protocol,
+			}},
+			Type:       serviceType,
+			Selector:   selector,
+			IPFamilies: ipFamilies,
+		},
+	}
+	for _, mutator := range mutators {
+		mutator(&service)
+	}
+	return data.clientset.CoreV1().Services(namespace).Create(context.TODO(), &service, metav1.CreateOptions{})
+}
+
+func (data *TestData) createPod(namespace, podName, containerName, image string, protocol corev1.Protocol, containerPort int32,
+	mutators ...func(pod *corev1.Pod)) (*corev1.Pod, error) {
+	privileged := true
+	pod := corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        podName,
+			Namespace:   namespace,
+			Annotations: map[string]string{},
+			Labels: map[string]string{
+				"nsx-op-e2e": podName,
+				"app":        podName,
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:            containerName,
+					Image:           image,
+					ImagePullPolicy: corev1.PullIfNotPresent,
+					SecurityContext: &corev1.SecurityContext{
+						Privileged: &privileged,
+					},
+					Ports: []corev1.ContainerPort{
+						{
+							Protocol:      protocol,
+							ContainerPort: containerPort,
+						},
+					},
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyNever,
+			HostNetwork:   false,
+			// Set it to 1s for immediate shutdown to reduce test run time and to avoid affecting subsequent tests.
+			TerminationGracePeriodSeconds: ptr.To[int64](1),
+		},
+	}
+	for _, mutator := range mutators {
+		mutator(&pod)
+	}
+	return data.clientset.CoreV1().Pods(namespace).Create(context.TODO(), &pod, metav1.CreateOptions{})
+}
+
+func (data *TestData) serviceWaitFor(readyTime time.Duration, namespace string, name string, conditionFunc func(svc *corev1.Service) (bool, error)) (*corev1.Service, error) {
+	err := wait.PollUntilContextTimeout(context.TODO(), 1*time.Second, readyTime, false, func(ctx context.Context) (bool, error) {
+		if svc, err := data.clientset.CoreV1().Services(namespace).Get(context.TODO(), name, metav1.GetOptions{}); err != nil {
+			if errors.IsNotFound(err) {
+				return false, nil
+			}
+			return false, fmt.Errorf("error when getting Service '%s/%s': %v", namespace, name, err)
+		} else {
+			return conditionFunc(svc)
+		}
+	})
+	if err != nil {
+		return nil, err
+	}
+	return data.clientset.CoreV1().Services(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+}
+
+func (data *TestData) deleteService(nsName string, svcName string) error {
+	ctx := context.TODO()
+	err := data.clientset.CoreV1().Services(nsName).Delete(ctx, svcName, metav1.DeleteOptions{})
+	if err != nil {
+		log.Error(err, "Failed to delete Service", "namespace", nsName, "name", svcName)
+	}
 	return err
 }
