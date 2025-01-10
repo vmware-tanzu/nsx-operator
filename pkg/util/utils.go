67ac6948f33fbc3de5e6cdde8bff7a884bdb1523
Wenying Dong <wenyingd@vmware.com>
2025-01-10 01:09:24 -0800

    New string generator for the hash string in NSX resources (#995) (#997)
    1. Use chars 0-9,a-z,A-Z to generate the hash string for ID/DisplayName in NSX
    resources with VPC scenario. It also works on the Security Policy related
    resources' display name in T1.
    2. The length is 6 chars when using the new hash string function.

--- pkg/util/utils.go
+++ pkg/util/utils.go
@@ -9,6 +9,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"math/big"
 	"net"
 	"strconv"
 	"strings"
@@ -31,6 +32,7 @@ import (
 
 const (
 	wcpSystemResource = "vmware-system-shared-t1"
+	base62Chars       = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 )
 
 var (
@@ -39,44 +41,47 @@ var (
 
 var log = &logger.Log
 
+func truncateLabelHash(data string) string {
+	return Sha1(data)[:common.HashLength]
+}
 func NormalizeLabels(matchLabels *map[string]string) *map[string]string {
 	newLabels := make(map[string]string)
 	for k, v := range *matchLabels {
-		newLabels[NormalizeLabelKey(k)] = NormalizeName(v)
+		newLabels[NormalizeLabelKey(k, truncateLabelHash)] = NormalizeName(v, truncateLabelHash)
 	}
 	return &newLabels
 }
 
-func NormalizeLabelKey(key string) string {
+func NormalizeLabelKey(key string, shaFn func(data string) string) string {
 	if len(key) <= common.MaxTagScopeLength {
 		return key
 	}
 	splitted := strings.Split(key, "/")
 	key = splitted[len(splitted)-1]
-	return normalizeNameByLimit(key, "", common.MaxTagScopeLength)
+	return normalizeNameByLimit(key, "", common.MaxTagScopeLength, shaFn)
 }
 
-func NormalizeName(name string) string {
-	return normalizeNameByLimit(name, "", common.MaxTagValueLength)
+func NormalizeName(name string, shaFn func(data string) string) string {
+	return normalizeNameByLimit(name, "", common.MaxTagValueLength, shaFn)
 }
 
-func normalizeNameByLimit(name string, suffix string, limit int) string {
+func normalizeNameByLimit(name string, suffix string, limit int, hashFn func(data string) string) string {
 	newName := connectStrings(common.ConnectorUnderline, name, suffix)
 	if len(newName) <= limit {
 		return newName
 	}
 
-	var hashString string
+	hashedTarget := name
 	if len(suffix) > 0 {
-		hashString = Sha1(suffix)
-	} else {
-		hashString = Sha1(name)
+		hashedTarget = suffix
 	}
-	nameLength := limit - common.HashLength - 1
+
+	hashString := hashFn(hashedTarget)
+	nameLength := limit - len(hashString) - 1
 	if len(name) < nameLength {
 		nameLength = len(name)
 	}
-	return strings.Join([]string{name[:nameLength], hashString[:common.HashLength]}, common.ConnectorUnderline)
+	return strings.Join([]string{name[:nameLength], hashString}, common.ConnectorUnderline)
 }
 
 func NormalizeId(name string) string {
@@ -94,10 +99,36 @@ func NormalizeId(name string) string {
 }
 
 func Sha1(data string) string {
+	sum := getSha1Bytes(data)
+	return fmt.Sprintf("%x", sum)
+}
+
+func getSha1Bytes(data string) []byte {
 	h := sha1.New() // #nosec G401: not used for security purposes
 	h.Write([]byte(data))
 	sum := h.Sum(nil)
-	return fmt.Sprintf("%x", sum)
+	return sum
+}
+
+// Sha1WithBase62 uses the chars in `base62Chars` to present the hash result on the input data. We now use Sha1 as
+// the hash algorithm.
+func Sha1WithBase62(data string) string {
+	sum := getSha1Bytes(data)
+	value := new(big.Int).SetBytes(sum[:])
+	base := big.NewInt(int64(len(base62Chars)))
+	var result []byte
+	for value.Cmp(big.NewInt(0)) > 0 {
+		mod := new(big.Int).Mod(value, base)
+		result = append(result, base62Chars[mod.Int64()])
+		value.Div(value, base)
+	}
+
+	// Reverse the result because the encoding process generates characters in reverse order
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	return string(result)
 }
 
 func RemoveDuplicateStr(strSlice []string) []string {
@@ -339,11 +370,15 @@ func UpdateK8sResourceAnnotation(client client.Client, ctx context.Context, k8sO
 	return nil
 }
 
+func truncateNameOrIDHash(data string) string {
+	return Sha1WithBase62(data)[:common.Base62HashLength]
+}
+
 // GenerateIDByObject generate string id for NSX resource using the provided Object's name and uid. Note,
 // this function is used on the resources with VPC scenario, and the provided obj is the K8s CR which is
 // used to generate the NSX resource.
 func GenerateIDByObject(obj metav1.Object) string {
-	return normalizeNameByLimit(obj.GetName(), string(obj.GetUID()), common.MaxIdLength)
+	return normalizeNameByLimit(obj.GetName(), string(obj.GetUID()), common.MaxIdLength, truncateNameOrIDHash)
 }
 
 // GenerateIDByObjectByLimit generate string id for NSX resource using the provided Object's name and uid,
@@ -352,13 +387,13 @@ func GenerateIDByObjectByLimit(obj metav1.Object, limit int) string {
 	if limit == 0 {
 		limit = common.MaxIdLength
 	}
-	return normalizeNameByLimit(obj.GetName(), string(obj.GetUID()), limit)
+	return normalizeNameByLimit(obj.GetName(), string(obj.GetUID()), limit, truncateNameOrIDHash)
 }
 
 func GenerateIDByObjectWithSuffix(obj metav1.Object, suffix string) string {
 	limit := common.MaxIdLength
 	limit -= len(suffix) + 1
-	return connectStrings(common.ConnectorUnderline, normalizeNameByLimit(obj.GetName(), string(obj.GetUID()), limit), suffix)
+	return connectStrings(common.ConnectorUnderline, normalizeNameByLimit(obj.GetName(), string(obj.GetUID()), limit, truncateNameOrIDHash), suffix)
 }
 
 // GenerateID generate id for NSX resource, some resources has complex index, so set it type to string
@@ -391,7 +426,7 @@ func GenerateTruncName(limit int, resName string, prefix, suffix, project, clust
 	}
 	oldName := generateDisplayName(common.ConnectorUnderline, resName, "", "", project, cluster)
 	if len(oldName) > adjustedLimit {
-		newName := normalizeNameByLimit(oldName, "", adjustedLimit)
+		newName := normalizeNameByLimit(oldName, "", adjustedLimit, truncateNameOrIDHash)
 		return generateDisplayName(common.ConnectorUnderline, newName, prefix, suffix, "", "")
 	}
 	return generateDisplayName(common.ConnectorUnderline, resName, prefix, suffix, project, cluster)
