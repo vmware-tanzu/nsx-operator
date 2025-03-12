7b616f883f8640a38eb5c8a084675da038f0aeb9
Deng Yun <yun.deng@broadcom.com>
2025-03-12 10:23:04 +0800

    Enhance pod controller status update (#1034)
    During the pod controller reconciles pod, it's helpful to expose error condition messages to the pod condition
    status, this helps users to understand what are the detailed error info.

--- pkg/controllers/pod/pod_controller.go
+++ pkg/controllers/pod/pod_controller.go
@@ -7,11 +7,13 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"reflect"
 	"time"
 
 	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	apimachineryruntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/record"
@@ -43,6 +45,82 @@ type PodReconciler struct {
 	NodeServiceReader servicecommon.NodeServiceReader
 	Recorder          record.EventRecorder
 	StatusUpdater     common.StatusUpdater
+}
+
+func setPodReadyStatusFalse(client client.Client, ctx context.Context, obj client.Object, transitionTime metav1.Time, err error, args ...interface{}) {
+	pod := obj.(*v1.Pod)
+	newConditions := []v1.PodCondition{
+		{
+			Type:   v1.PodReady,
+			Status: v1.ConditionFalse,
+			Message: fmt.Sprintf(
+				"error occurred while processing the Pod. Error: %v",
+				err,
+			),
+			Reason:             "PodNotReady",
+			LastTransitionTime: transitionTime,
+		},
+	}
+	updatePodStatusConditions(client, ctx, pod, newConditions)
+}
+
+func setPodReadyStatusTrue(client client.Client, ctx context.Context, obj client.Object, transitionTime metav1.Time, args ...interface{}) {
+	pod := obj.(*v1.Pod)
+	newConditions := []v1.PodCondition{
+		{
+			Type:               v1.PodReady,
+			Status:             v1.ConditionTrue,
+			Message:            "Pod has been successfully created/updated",
+			Reason:             "PodReady",
+			LastTransitionTime: transitionTime,
+		},
+	}
+	updatePodStatusConditions(client, ctx, pod, newConditions)
+}
+
+func updatePodStatusConditions(client client.Client, ctx context.Context, pod *v1.Pod, newConditions []v1.PodCondition) {
+	conditionsUpdated := false
+	for i := range newConditions {
+		if mergePodStatusCondition(pod, &newConditions[i]) {
+			conditionsUpdated = true
+		}
+	}
+
+	if conditionsUpdated {
+		err := client.Status().Update(ctx, pod)
+		if err != nil {
+			log.Error(err, "Failed to update Pod status", "Name", pod.Name, "Namespace", pod.Namespace)
+		}
+		log.V(2).Info("Updated pod", "Name", pod.Name, "Namespace", pod.Namespace,
+			"New Conditions", newConditions)
+	}
+}
+
+func mergePodStatusCondition(pod *v1.Pod, newCondition *v1.PodCondition) bool {
+	matchedCondition := getExistingConditionOfType(newCondition.Type, pod.Status.Conditions)
+
+	if reflect.DeepEqual(matchedCondition, newCondition) {
+		log.V(2).Info("Conditions already match", "New Condition", newCondition, "Existing Condition", matchedCondition)
+		return false
+	}
+
+	if matchedCondition != nil {
+		matchedCondition.Reason = newCondition.Reason
+		matchedCondition.Message = newCondition.Message
+		matchedCondition.Status = newCondition.Status
+	} else {
+		pod.Status.Conditions = append(pod.Status.Conditions, *newCondition)
+	}
+	return true
+}
+
+func getExistingConditionOfType(conditionType v1.PodConditionType, existingConditions []v1.PodCondition) *v1.PodCondition {
+	for i := range existingConditions {
+		if existingConditions[i].Type == conditionType {
+			return &existingConditions[i]
+		}
+	}
+	return nil
 }
 
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
@@ -77,6 +155,7 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 		isExisting, nsxSubnetPath, err := r.GetSubnetPathForPod(ctx, pod)
 		if err != nil {
 			log.Error(err, "failed to get NSX resource path from subnet", "pod.Name", pod.Name, "pod.UID", pod.UID)
+			r.StatusUpdater.UpdateFail(ctx, pod, err, "", setPodReadyStatusFalse)
 			return common.ResultRequeue, err
 		}
 		if !isExisting {
@@ -92,14 +171,15 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 		contextID := *node.UniqueId
 		nsxSubnet, err := r.SubnetService.GetSubnetByPath(nsxSubnetPath)
 		if err != nil {
+			r.StatusUpdater.UpdateFail(ctx, pod, err, "", setPodReadyStatusFalse)
 			return common.ResultRequeue, err
 		}
 		_, err = r.SubnetPortService.CreateOrUpdateSubnetPort(pod, nsxSubnet, contextID, &pod.ObjectMeta.Labels)
 		if err != nil {
-			r.StatusUpdater.UpdateFail(ctx, pod, err, "", nil)
+			r.StatusUpdater.UpdateFail(ctx, pod, err, "", setPodReadyStatusFalse)
 			return common.ResultRequeue, err
 		}
-		r.StatusUpdater.UpdateSuccess(ctx, pod, nil)
+		r.StatusUpdater.UpdateSuccess(ctx, pod, setPodReadyStatusTrue)
 	} else {
 		subnetPortID := r.SubnetPortService.BuildSubnetPortId(&pod.ObjectMeta)
 		if err := r.SubnetPortService.DeleteSubnetPortById(subnetPortID); err != nil {
