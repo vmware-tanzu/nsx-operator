TaoZou1/nsx-operator-1

Using pointer in the pending queue
Fix one crash issue in build.go
Using pointer in the pending queue
Start inventory controller in the main

Test Done:
1. run the manger to check if it not crash

cmd/main.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/legacy/v1alpha1"
 	crdv1alpha1 "github.com/vmware-tanzu/nsx-operator/pkg/apis/vpc/v1alpha1"
+	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/inventory"
 	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/ipaddressallocation"
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/config"
@@ -42,6 +43,7 @@ import (
 	subnetbindingcontroller "github.com/vmware-tanzu/nsx-operator/pkg/controllers/subnetbinding"
 	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/subnetport"
 	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/subnetset"
+	inventoryservice "github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/inventory"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/ipblocksinfo"
 	nodeservice "github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/node"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/staticroute"
@@ -241,6 +243,8 @@ func startServiceController(mgr manager.Manager, nsxClient *nsx.Client) {
 			log.Error(err, "Failed to initialize SubnetConnectionBindingMap commonService")
 			os.Exit(1)
 		}
+		inventoryService := inventoryservice.NewInventoryService(commonService)
+		inventoryService.Initialize()
 
 		// Start controllers which only supports VPC
 		StartNetworkInfoController(mgr, vpcService, ipblocksInfoService)
@@ -281,6 +285,7 @@ func startServiceController(mgr manager.Manager, nsxClient *nsx.Client) {
 		networkpolicycontroller.StartNetworkPolicyController(mgr, commonService, vpcService)
 		service.StartServiceLbController(mgr, commonService)
 		subnetbindingcontroller.StartSubnetBindingController(mgr, subnetService, subnetBindingService)
+		inventory.StartupInventoryController(mgr, inventoryService, cf)
 	}
 	// Start controllers which can run in non-VPC mode
 	securitypolicycontroller.StartSecurityPolicyController(mgr, commonService, vpcService)

pkg/nsx/services/inventory/inventory.go
@@ -202,7 +202,7 @@ func (s *InventoryService) DeleteResource(externalId string, resourceType Invent
 		deletedInfo["external_id"] = externalId
 		s.requestBuffer = append(s.requestBuffer, containerinventory.ContainerInventoryObject{ContainerObject: deletedInfo,
 			ObjectUpdateType: operationDelete})
-		s.pendingDelete[externalId] = inventoryObject
+		s.pendingDelete[externalId] = inventoryObject.(*containerinventory.ContainerApplicationInstance)
 
 		if resourceType == ContainerApplicationInstance {
 			return s.DeleteContainerApplicationInstance(externalId, inventoryObject.(*containerinventory.ContainerApplicationInstance))
@@ -235,11 +235,11 @@ func (s *InventoryService) sendNSXRequestAndUpdateInventoryStore() error {
 func (s *InventoryService) updateInventoryStore() error {
 	log.Info("Update Inventory store after NSX request succeeds")
 	for _, addItem := range s.pendingAdd {
-		switch reflect.ValueOf(addItem).FieldByName("ResourceType").String() {
+		switch reflect.ValueOf(addItem).Elem().FieldByName("ResourceType").String() {
 
 		case string(ContainerApplicationInstance):
-			instacne := addItem.(containerinventory.ContainerApplicationInstance)
-			err := s.applicationInstanceStore.Add(&instacne)
+			instacne := addItem.(*containerinventory.ContainerApplicationInstance)
+			err := s.applicationInstanceStore.Add(instacne)
 			if err != nil {
 				return err
 			}
@@ -247,10 +247,10 @@ func (s *InventoryService) updateInventoryStore() error {
 		}
 	}
 	for _, deleteItem := range s.pendingDelete {
-		switch reflect.ValueOf(deleteItem).FieldByName("ResourceType").String() {
+		switch reflect.ValueOf(deleteItem).Elem().FieldByName("ResourceType").String() {
 		case string(ContainerApplicationInstance):
-			instance := deleteItem.(containerinventory.ContainerApplicationInstance)
-			err := s.applicationInstanceStore.Delete(&instance)
+			instance := deleteItem.(*containerinventory.ContainerApplicationInstance)
+			err := s.applicationInstanceStore.Delete(instance)
 			if err != nil {
 				return err
 			}

vmware-tanzu/nsx-operator

cmd/main.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/legacy/v1alpha1"
 	crdv1alpha1 "github.com/vmware-tanzu/nsx-operator/pkg/apis/vpc/v1alpha1"
+	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/inventory"
 	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/ipaddressallocation"
 
 	"github.com/vmware-tanzu/nsx-operator/pkg/config"
@@ -42,6 +43,7 @@ import (
 	subnetbindingcontroller "github.com/vmware-tanzu/nsx-operator/pkg/controllers/subnetbinding"
 	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/subnetport"
 	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/subnetset"
+	inventoryservice "github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/inventory"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/ipblocksinfo"
 	nodeservice "github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/node"
 	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/staticroute"
@@ -241,6 +243,8 @@ func startServiceController(mgr manager.Manager, nsxClient *nsx.Client) {
 			log.Error(err, "Failed to initialize SubnetConnectionBindingMap commonService")
 			os.Exit(1)
 		}
+		inventoryService := inventoryservice.NewInventoryService(commonService)
+		inventoryService.Initialize()
 
 		// Start controllers which only supports VPC
 		StartNetworkInfoController(mgr, vpcService, ipblocksInfoService)
@@ -281,6 +285,7 @@ func startServiceController(mgr manager.Manager, nsxClient *nsx.Client) {
 		networkpolicycontroller.StartNetworkPolicyController(mgr, commonService, vpcService)
 		service.StartServiceLbController(mgr, commonService)
 		subnetbindingcontroller.StartSubnetBindingController(mgr, subnetService, subnetBindingService)
+		inventory.StartInventoryController(mgr, inventoryService, cf)
 	}
 	// Start controllers which can run in non-VPC mode
 	securitypolicycontroller.StartSecurityPolicyController(mgr, commonService, vpcService)

pkg/nsx/services/inventory/inventory.go
@@ -202,7 +202,7 @@ func (s *InventoryService) DeleteResource(externalId string, resourceType Invent
 		deletedInfo["external_id"] = externalId
 		s.requestBuffer = append(s.requestBuffer, containerinventory.ContainerInventoryObject{ContainerObject: deletedInfo,
 			ObjectUpdateType: operationDelete})
-		s.pendingDelete[externalId] = inventoryObject
+		s.pendingDelete[externalId] = inventoryObject.(*containerinventory.ContainerApplicationInstance)
 
 		if resourceType == ContainerApplicationInstance {
 			return s.DeleteContainerApplicationInstance(externalId, inventoryObject.(*containerinventory.ContainerApplicationInstance))
@@ -235,11 +235,11 @@ func (s *InventoryService) sendNSXRequestAndUpdateInventoryStore() error {
 func (s *InventoryService) updateInventoryStore() error {
 	log.Info("Update Inventory store after NSX request succeeds")
 	for _, addItem := range s.pendingAdd {
-		switch reflect.ValueOf(addItem).FieldByName("ResourceType").String() {
+		switch reflect.ValueOf(addItem).Elem().FieldByName("ResourceType").String() {
 
 		case string(ContainerApplicationInstance):
-			instacne := addItem.(containerinventory.ContainerApplicationInstance)
-			err := s.applicationInstanceStore.Add(&instacne)
+			instance := addItem.(*containerinventory.ContainerApplicationInstance)
+			err := s.applicationInstanceStore.Add(instance)
 			if err != nil {
 				return err
 			}
@@ -247,10 +247,10 @@ func (s *InventoryService) updateInventoryStore() error {
 		}
 	}
 	for _, deleteItem := range s.pendingDelete {
-		switch reflect.ValueOf(deleteItem).FieldByName("ResourceType").String() {
+		switch reflect.ValueOf(deleteItem).Elem().FieldByName("ResourceType").String() {
 		case string(ContainerApplicationInstance):
-			instance := deleteItem.(containerinventory.ContainerApplicationInstance)
-			err := s.applicationInstanceStore.Delete(&instance)
+			instance := deleteItem.(*containerinventory.ContainerApplicationInstance)
+			err := s.applicationInstanceStore.Delete(instance)
 			if err != nil {
 				return err
 			}