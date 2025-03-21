a8e46f27afe9ddd13e79e63efaa6b3f5fcc0f01d
Xie Zheng <xie.zheng@broadcom.com>
2025-03-21 14:38:44 +0800

    Implement node inventory controller

---
+++ pkg/controllers/inventory/node_handler.go
@@ -0,0 +1,61 @@
+package inventory
+
+import (
+	"context"
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/cache"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/inventory"
+)
+
+func watchNode(c *InventoryController, mgr ctrl.Manager) error {
+	nodeInformer, err := mgr.GetCache().GetInformer(context.Background(), &v1.Node{})
+	if err != nil {
+		log.Error(err, "Failed to create Node informer")
+		return err
+	}
+
+	_, err = nodeInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			// Handle Node add event
+			c.handleNode(obj)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			// Handle Node update event
+			c.handleNode(newObj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			// Handle Node delete event
+			c.handleNode(obj)
+		},
+	})
+	if err != nil {
+		log.Error(err, "Failed to add Node event handler")
+		return err
+	}
+	return nil
+}
+
+func (c *InventoryController) handleNode(obj interface{}) {
+	var node *v1.Node
+	ok := false
+	switch obj1 := obj.(type) {
+	case *v1.Node:
+		node = obj1
+	case cache.DeletedFinalStateUnknown:
+		node, ok = obj1.Obj.(*v1.Node)
+		if !ok {
+			err := fmt.Errorf("obj is not valid *v1.Node")
+			log.Error(err, "DeletedFinalStateUnknown Obj is not *v1.Node")
+			return
+		}
+	}
+	log.V(1).Info("Inventory processing Node", "name", node.Name)
+	// key is ObjectName{Namespace: "", Name: obj.GetName()}
+	key, _ := keyFunc(node)
+	log.V(1).Info("Adding Node key to inventory object queue", "Node key", key)
+	c.inventoryObjectQueue.Add(inventory.InventoryKey{InventoryType: inventory.ContainerClusterNode, ExternalId: string(node.UID), Key: key})
+}
