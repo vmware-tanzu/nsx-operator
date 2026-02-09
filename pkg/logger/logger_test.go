9eab9833cc5f2a0401e52300de382f7f8572f598
zhengxiexie <xie.zheng@broadcom.com>
2026-02-09 10:50:29 +0800

    Add configurable `--log-color` flag to enable/disable ANSI log colors
    - Updated `ZapCustomLogger` to accept `enableColor` flag instead of relying on log level.
    - Added `--log-color` flag to main programs and test frameworks for explicit control.
    - Updated tests to validate color handling based on the flag setting.
    
    Change-Id: I80a900072a535f0879f82ea99a90d68763da2aaf

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -1,8 +1,13 @@
 package logger
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/rs/zerolog"
 )
 
 func TestZapLoggerLevels(t *testing.T) {
@@ -44,7 +49,7 @@ func TestZapLoggerLevels(t *testing.T) {
 			t.Logf("Testing: %s", tc.description)
 
 			// Create logger with specified configuration
-			logger := ZapCustomLogger(tc.debug, tc.logLevel).Logger
+			logger := ZapCustomLogger(tc.debug, tc.logLevel, false).Logger
 
 			// Test various log levels
 			logger.Info("This is an info message", "test_case", tc.name, "timestamp", time.Now().Format("15:04:05"))
@@ -68,7 +73,7 @@ func TestZapLoggerLevels(t *testing.T) {
 func TestCustomLogger(t *testing.T) {
 	t.Log("Testing CustomLogger with all log levels...")
 	// Test CustomLogger wrapper
-	customLogger := ZapCustomLogger(true, 2)
+	customLogger := ZapCustomLogger(true, 2, false)
 
 	// Test all five log levels
 	customLogger.Trace("This is a trace message", "test_case", "trace_test", "timestamp", time.Now().Format("15:04:05"))
@@ -78,4 +83,77 @@ func TestCustomLogger(t *testing.T) {
 	customLogger.Error(nil, "This is an error message", "test_case", "error_test", "timestamp", time.Now().Format("15:04:05"))
 
 	t.Log("CustomLogger test completed - verify all log levels are displayed with proper formatting and colors")
+}
+
+func TestGetLogLevel(t *testing.T) {
+	testCases := []struct {
+		name     string
+		debug    bool
+		logLevel int
+		expected int
+	}{
+		{"default", false, 0, 0},
+		{"debug_flag_only", true, 0, 2},
+		{"log_level_1", false, 1, 1},
+		{"log_level_3", false, 3, 3},
+		{"debug_with_higher_level", true, 3, 3},
+		{"debug_with_lower_level", true, 1, 2},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := getLogLevel(tc.debug, tc.logLevel)
+			if got != tc.expected {
+				t.Errorf("getLogLevel(%v, %d) = %d, want %d", tc.debug, tc.logLevel, got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestAnsiColorControlledByFlag(t *testing.T) {
+	// Helper: create a console writer via the same logic as ZapCustomLogger,
+	// but write to a buffer so we can inspect the output.
+	buildWriter := func(enableColor bool) (*bytes.Buffer, zerolog.Logger) {
+		buf := &bytes.Buffer{}
+		cw := zerolog.ConsoleWriter{
+			Out:        buf,
+			NoColor:    !enableColor,
+			TimeFormat: logTmFmtWithMS,
+			FormatLevel: func(i interface{}) string {
+				levelStr := strings.ToUpper(fmt.Sprintf("%s", i))
+				if !enableColor {
+					return levelStr
+				}
+				switch levelStr {
+				case "INFO":
+					return colorGreen + levelStr + colorReset
+				case "WARN":
+					return colorMagenta + levelStr + colorReset
+				case "ERROR":
+					return colorBrightRed + levelStr + colorReset
+				default:
+					return levelStr
+				}
+			},
+		}
+		l := zerolog.New(cw).Level(zerolog.InfoLevel)
+		return buf, l
+	}
+
+	t.Run("color_disabled", func(t *testing.T) {
+		buf, l := buildWriter(false)
+		l.Info().Msg("hello")
+		output := buf.String()
+		if strings.Contains(output, "\033[") {
+			t.Errorf("color=false should not contain ANSI codes, got: %q", output)
+		}
+	})
+
+	t.Run("color_enabled", func(t *testing.T) {
+		buf, l := buildWriter(true)
+		l.Info().Msg("hello")
+		output := buf.String()
+		if !strings.Contains(output, "\033[") {
+			t.Errorf("color=true should contain ANSI codes, got: %q", output)
+		}
+	})
 }
